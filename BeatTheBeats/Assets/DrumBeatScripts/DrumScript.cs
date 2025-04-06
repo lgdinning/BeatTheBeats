c78528c4249eff98469f2168779d0218443b3838
Elena Wang <elenawzy@gmail.com>
2025-04-05 22:45:39 -0700

    added drum beat bare bones

---
+++ BeatTheBeats/Assets/DrumBeatScripts/DrumScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class DrumScript : MonoBehaviour
+{
+    public Sprite drumNotHit;
+    public Sprite drumHit;
+
+    private int drumHitCount = 0;
+    private int clearHitCount = 20;
+    private SpriteRenderer spriteRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // E pressed
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            drumHitCount++;
+            spriteRenderer.sprite = drumHit;
+            Debug.Log("E pressed! Total count: " + drumHitCount);
+            if (drumHitCount >= clearHitCount)
+            {
+                Debug.Log("LIMMIT REACHED!!! " + drumHitCount);
+            }
+        }
+        // E not pressed anymore
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            spriteRenderer.sprite = drumNotHit;
+        }
+    }
+}
